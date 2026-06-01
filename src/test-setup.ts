6c36e350fb9758a73a92b23bf45ede0a261fba46
lucas.matricarde <lucas.eduardo@wises.com.br>
2026-05-31 23:55:48 -0300

    test: add window.matchMedia mock to fix GSAP ScrollTrigger in jsdom

---
+++ src/test-setup.ts
@@ -0,0 +1,14 @@
+// Mock window.matchMedia for GSAP ScrollTrigger in jsdom test environment
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string): MediaQueryList => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
